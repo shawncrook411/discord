c8a8782b59a2d5d89b96dbbaed64fd9384f708f8
Shawn Crook <shawncrook411@gmail.com>
2025-10-29 23:56:19 -0600

    Basic refactoring

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -7,11 +7,11 @@ module.exports = {
         `
             Here are all the commands!
 
-            - /help - displays the available commands
-            - /lorem - displays words from lorem ipsum
-            - /die - kills the bot :(
-            - /roll - returns a random number between 1 and the chosen die size
-            - /game - starts a game of tic tac toe
+            /help - displays the available commands
+            /lorem - displays words from lorem ipsum
+            /die - kills the bot :(
+            /roll - returns a random number between 1 and the chosen die size
+            /game - starts a game of tic tac toe
         `
         );
     }
--- index.js
+++ index.js
@@ -1,50 +1,35 @@
-const fs = require('fs');
-const path = require('path');
-
-const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, } = require('discord.js');
 
 const dotenv = require('dotenv');
 dotenv.config();
 
-
-//
 const token = process.env.token;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+// Creates and loads commands into client
+const commandPath = './commands/utility/'
+const { commandsList } = require('./deploy-commands.js')
 
-for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+commandsList.forEach((file) =>  {
+    const command = require(`${commandPath}${file}.js`)
+    client.commands.set(command.data.name, command)
+})
 
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
+// Creates events listeners
+const eventPath = './events/'
+const events = ['interactionCreate', 'ready']
 
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
-}
-
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
+events.forEach((file) => {
+    const event = require(`${eventPath}${file}.js`)
 
     if (event.once) {
         client.once(event.name, (...args) => event.execute(...args));
     } else {
         client.on(event.name, (...args) => event.execute(...args));
     }
-}
+})
 
 client.login(token);
 
