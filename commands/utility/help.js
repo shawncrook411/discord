935f3417e262930147dab02ee364ba2d234a86e8
Shawn Crook <shawncrook411@gmail.com>
2025-10-29 21:46:50 -0600

    finished lorem command

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -4,15 +4,15 @@ module.exports = {
     data: new SlashCommandBuilder().setName('help').setDescription('Displays the available commands'),
     async execute(interaction) {
         await interaction.reply(
-`
-Here are all the commands!
+        `
+            Here are all the commands!
 
-- /help - displays the available commands
-- /lorem - displays words from lorem ipsum
-- /die - kills the bot :(
-- /roll - returns a random number between 1 and the chosen die size
-- /game - starts a game of tic tac toe
-`
+            - /help - displays the available commands
+            - /lorem - displays words from lorem ipsum
+            - /die - kills the bot :(
+            - /roll - returns a random number between 1 and the chosen die size
+            - /game - starts a game of tic tac toe
+        `
         );
     }
 }
--- commands/utility/lorem.js
+++ commands/utility/lorem.js
@@ -1,20 +1,15 @@
 const { SlashCommandBuilder } = require ('discord.js');
-const { readFile } = require('fs');
+const { readFileSync } = require('fs');
 
-const random = Math.floor(Math.random() * 1000)
+const random = Math.floor(Math.random() * 100)
 
-const lorem = readFile('lorem.txt', (err, data) => {
-    return data
-});
-
+const lorem = readFileSync('./commands/utility/lorem.txt', 'utf8').split(' ').slice(0, random).join(" ")
 
 module.exports = {
     data: new SlashCommandBuilder().setName('lorem').setDescription('Displays words from lorem ipsum'),
     async execute(interaction) {
         await interaction.reply(
-            lorem.split(' ').slice(0, random)
+            lorem
         );
     }
 }
-
-
